61637787862eff1f99deeb9225d6a18edcaf79b0
andrevmarkelov <andrev_markelov@mail.ru>
2024-11-25 21:00:10 +0300

    adding error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,8 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>404</h1>
-        <a href="{{ route('home') }}">На главную</a>
+    <div class="container py-5 min-vh-100">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <img src="{{ asset('assets/images/404.webp') }}" alt="404" class="w-100 mb-5 rounded-4">
+
+                <h1 class="h3 color-dark-blue text-center mb-2">Страница не найдена</h1>
+                <p class="color-grey text-center">К сожалению, страница, которую вы ищете, не существует. Проверьте правильность введённого адреса или
+                    перейдите на</p>
+                <div class="d-flex justify-content-center">
+                    <a href="{{ route('home') }}" class="btn-default">Главную страницу</a>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
